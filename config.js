8a9eeb1a49ab557feca171f453a858048e984bae
Emirqw3 <snowvlox@gmail.com>
2024-08-16 14:39:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    token: "MTI3Mzk1OTQ4MDIwMzQxMTUyOQ.G_4o1y.At0cGHwr3eQSUj8xOTA5_NYW7D85mdY5J7_C4s", 
+    token: "MTI3Mzk1OTQ4MDIwMzQxMTUyOQ.GNQV1_.Ajga_d9HTdW93s20xOLfWedmwErcpEpK1JQ7W4", 
     prefix: "b!",
     botdavet: "discord.gg/bombot",
     desteksunucusu: "discord.gg/bombot",
     website: "discord.gg/bombot",
     topgg: "discord.gg/bombot",
     politika: "discord.gg/bombot",
-    sunucuid: "discord.gg/bombot",
-    destekçi: "discord.gg/bombot"
+    sunucuid: "1267444348694298680",
+    destekçi: "1273027453035610203 "
 }
 
